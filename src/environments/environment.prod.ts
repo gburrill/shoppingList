ead10dc2e05721b3d5e5ce62975de8558ca86bd8
gburrill <gabrielburrill@gmail.com>
2017-09-17 23:55:12 +1000

    firebase deploy 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBv1z7-UJOOjOY3FwAOrOhz0s0H7bFrUeQ',
+    authDomain: 'shoppinglist-df0cd.firebaseapp.com',
+    databaseURL: 'https://shoppinglist-df0cd.firebaseio.com',
+    projectId: 'shoppinglist-df0cd',
+    storageBucket: 'shoppinglist-df0cd.appspot.com',
+    messagingSenderId: '516205529058'
+  }
 };
