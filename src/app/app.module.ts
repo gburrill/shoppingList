74830afa6328689913fcbcf9441e0a99efa21cd3
gburrill <gabrielburrill@gmail.com>
2017-09-17 23:20:26 +1000

    angularfire set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase, 'shoppingList'), // imports firebase/app needed for everything
+    AngularFireDatabaseModule, // imports firebase/database, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBv1z7-UJOOjOY3FwAOrOhz0s0H7bFrUeQ',
+    authDomain: 'shoppinglist-df0cd.firebaseapp.com',
+    databaseURL: 'https://shoppinglist-df0cd.firebaseio.com',
+    projectId: 'shoppinglist-df0cd',
+    storageBucket: 'shoppinglist-df0cd.appspot.com',
+    messagingSenderId: '516205529058'
+  }
 };
