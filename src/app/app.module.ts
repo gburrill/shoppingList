03cd820b22aa719e163bf7e63a2039975d9ce18e
gburrill <gabrielburrill@gmail.com>
2017-09-19 22:54:09 +1000

    set up ngx-bootstrap, created navbar and routed to it from app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,24 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CollapseModule } from 'ngx-bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'shoppingList'), // imports firebase/app needed for everything
     AngularFireDatabaseModule, // imports firebase/database, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    CollapseModule, // Collapsible navbars
+    AlertModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
